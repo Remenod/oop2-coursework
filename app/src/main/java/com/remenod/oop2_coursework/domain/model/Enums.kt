e84b14be1fc2e451e41d1e29282deb2d6f4efc95
Remenod <remenod@gmail.com>
2026-05-25 23:28:57 +0300

    add enums

---
+++ app/src/main/java/com/remenod/oop2_coursework/domain/model/Enums.kt
@@ -0,0 +1,31 @@
+package com.remenod.oop2_coursework.domain.model
+
+enum class WorkStatus {
+    CREATED,
+    IN_PROGRESS,
+    BLOCKED,
+    DONE,
+    CANCELLED
+}
+
+enum class Priority {
+    LOW,
+    NORMAL,
+    HIGH,
+    CRITICAL
+}
+
+enum class SubmissionStatus {
+    NOT_SUBMITTED,
+    DRAFT,
+    SUBMITTED,
+    LATE,
+    GRADED
+}
+
+enum class AttachmentOpenMode {
+    LOCAL,
+    BROWSER,
+    CLOUD,
+    EXTERNAL_APP
+}
