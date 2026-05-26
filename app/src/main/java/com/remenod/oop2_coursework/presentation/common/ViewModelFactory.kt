6d7668d41388f688c53f0a0cb98b417c40174c70
Remenod <remenod@gmail.com>
2026-05-26 03:23:05 +0300

    disable stupid ass JVM warn

--- app/src/main/java/com/remenod/oop2_coursework/presentation/common/ViewModelFactory.kt
+++ app/src/main/java/com/remenod/oop2_coursework/presentation/common/ViewModelFactory.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider
 class ViewModelFactory<T : ViewModel>(
     private val creator: () -> T
 ) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST") // disable stupid ass JVM warn
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return creator() as T
     }
